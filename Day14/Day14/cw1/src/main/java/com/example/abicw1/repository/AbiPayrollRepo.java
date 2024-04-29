2bd5b6c1b0961a02bc160461991812b987480c1b
ABISHIYA R G <124857771+abishiyarg2004@users.noreply.github.com>
2024-04-29 10:35:48 +0530

    Delete Day14/Day14 directory

--- Day14/Day14/cw1/src/main/java/com/example/abicw1/repository/AbiPayrollRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.abicw1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.abicw1.model.AbiPayroll;
-
-public interface AbiPayrollRepo extends JpaRepository<AbiPayroll,Long>{
-    
-}
--- Day14/Day14/cw2/src/main/java/com/example/abicw2/repository/AbiBookRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.abicw2.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.abicw2.model.AbiBook;
-
-public interface AbiBookRepo extends JpaRepository<AbiBook, Long> {
-}
-

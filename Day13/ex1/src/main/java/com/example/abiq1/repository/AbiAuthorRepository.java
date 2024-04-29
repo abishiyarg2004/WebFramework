72c50e819a6cfb7d2c1bfb73fea96929f1f60d14
ABISHIYA R G <124857771+abishiyarg2004@users.noreply.github.com>
2024-04-29 11:47:42 +0530

    Delete Day13/ex1 directory

--- Day13/ex1/src/main/java/com/example/abiq1/repository/AbiAuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.abiq1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.abiq1.model.AbiAuthor;
-@Repository
-public interface AbiAuthorRepository extends JpaRepository<AbiAuthor, Long> {
-}
